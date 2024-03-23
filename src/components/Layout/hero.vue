57cbf67d7329db1ab68343c431a229fc63f7d9ab
Unclesam <adeyemis958@gmail.com>
2024-03-23 15:17:50 -0700

    update hero.vue

--- src/components/Layout/hero.vue
+++ src/components/Layout/hero.vue
@@ -1,43 +1,49 @@
 <template>
-    <section class=" dark:bg-gray-900">
-        <div class="grid max-w-screen-xl md:px-28 px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <span
-                    class="bg-green-300 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 mb-8 rounded dark:bg-green-900 dark:text-green-300">Web
-                    App Developer</span>
-                <h1
-                    class="max-w-2xl my-4 text-4xl font-medium tracking-tight text-white leading-none md:text-5xl xl:text-6xl dark:text-white">
-                    { Hello World! }</h1>
-                <h1
-                    class="max-w-2xl mb-4 text-4xl font-medium tracking-tight text-white leading-none md:text-5xl xl:text-6xl dark:text-white">
-                    I'm Samuel Adeyemi</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">I'm a creative web app developer with rock-solid experience to build simple and complex web applications with recent technologies.</p>
-                <a href="#"
-                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-green-500 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Download My CV
-                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </a>
-                <!-- <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+  <section class=" dark:bg-gray-900">
+    <div
+      class="grid order-1 md:order-1 max-w-screen-xl md:px-28 px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="md:hidden md:order-2 lg:mt-0 lg:col-span-5 md:flex">
+        <img src="@/assets/img/sam.png"
+          class="w-64 mx-auto my-6 mb-10 rounded-full border glow-neon-button border-green-600" alt="mockup">
+      </div>
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <span
+          class="bg-green-300 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 mb-8 rounded dark:bg-green-900 dark:text-green-300">Web
+          App Developer</span>
+        <h1
+          class="max-w-2xl my-4 text-4xl font-medium tracking-tight text-white leading-none md:text-5xl xl:text-6xl dark:text-white">
+          { Hello World! }</h1>
+        <h1
+          class="max-w-2xl mb-4 text-4xl font-medium tracking-tight text-white leading-none md:text-5xl xl:text-6xl dark:text-white">
+          I'm Samuel Adeyemi</h1>
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">I'm a
+          creative web app developer with rock-solid experience to build simple and complex web applications with recent
+          technologies.</p>
+        <a href="#"
+          class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-green-500 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+          Download My CV
+          <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"></path>
+          </svg>
+        </a>
+        <!-- <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Speak to Sales
             </a>  -->
-            </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="@/assets/img/sam.png" class="w-full rounded-full border glow-neon-button border-green-600" alt="mockup">
-            </div>
-        </div>
-    </section>
+      </div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <img src="@/assets/img/sam.png" class="w-full rounded-full border glow-neon-button border-green-600"
+          alt="mockup">
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 </script>
 
 <style>
-
 .glow-neon-button {
   position: relative;
 }
@@ -69,5 +75,4 @@
     opacity: 0;
   }
 }
-
 </style>
