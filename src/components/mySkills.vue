d69f92a5ef96ca49262ef6710791b40d9bc666e8
Unclesam <adeyemis958@gmail.com>
2024-04-30 08:09:04 -0700

    new

--- src/components/mySkills.vue
+++ src/components/mySkills.vue
@@ -5,7 +5,7 @@
 
             <div class="">
                 <div class="skills">
-                    <div class="grid grid-cols-2 md:grid-cols-6 space-y-4 items-center">
+                    <div class="grid grid-cols-2 md:grid-cols-6 space-y-4 justify-center items-center">
 
 
                         <div class="" data-aos="fade-in">
